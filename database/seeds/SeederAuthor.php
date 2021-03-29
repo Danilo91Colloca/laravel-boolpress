340b680638b1513505e44a483ddf7c6a7f78f019
Danilo <danilo.colloca91@gmail.com>
2021-03-29 18:04:39 +0200

    seed di un numero rando di post /commenti/autori

--- database/seeds/SeederAuthor.php
+++ database/seeds/SeederAuthor.php
@@ -15,32 +15,36 @@ class SeederAuthor extends Seeder
      */
     public function run(Faker $faker)
     {
-        $faker->addProvider(new WW\Faker\Provider\Picture($faker));
+            $faker->addProvider(new WW\Faker\Provider\Picture($faker));
+        for($i=0; $i<12; $i++){
+            $author = new Author();
+            $author->name=$faker->firstName();
+            $author->lastname=$faker->lastName();
+            $author->mail=$faker->email();
+            $author->save();
 
-        $author = new Author();
-        $author->name='ciccio';
-        $author->lastname='pinco';
-        $author->mail='ciccio@pinco';
-        $author->save();
+            $details = new DetailsAuthor();
+            $details->pic = 'qui ci saranno i dettagli';
+            $details->biography = $faker->text();
+            $details->instagram = $faker->url();
+            $details->twitter = $faker->url();
+            $details->facebook = $faker->url();
+            $details->linkedin = $faker->url();
 
-        $details = new DetailsAuthor();
-        $details->pic = 'qui ci saranno i dettagli';
-        $details->biography = 'qui ci saranno i dettagli';
-        $details->instagram = 'qui ci saranno i dettagli';
-        $details->twitter = 'qui ci saranno i dettagli';
-        $details->facebook = 'qui ci saranno i dettagli';
-        $details->linkedin = 'qui ci saranno i dettagli';
+            $author->detail()->save($details);
+            for($x=0; $x < rand(2, 4); $x++){
+                $post = new Post();
+                $post->post_title='Title of Post';
+                $post->paragraph=$faker->text();
+                $author->posts()->save($post);
 
-        $author->detail()->save($details);
-
-        $post = new Post();
-        $post->post_title='qui ci sara il titolo post';
-        $post->paragraph='qui ci sara blablbalbaldksdjfklajlsdogldfkgjv gdhjkals jadhfg djklsa ldkja s';
-        $author->posts()->save($post);
-
-        $comment = new Comment();
-        $comment->name='pippo';
-        $comment->comment_text='fkljdf lkjdsfkl adkls dfoiufoeis klndf aklsdjf ';
-        $post->comments()->save($comment);
+                for($y=0; $y < rand(0, 7); $y++ ) {
+                    $comment = new Comment();
+                    $comment->name=$faker->name();
+                    $comment->comment_text=$faker->text();
+                    $post->comments()->save($comment);
+                }
+            }
+        }
     }
 }
