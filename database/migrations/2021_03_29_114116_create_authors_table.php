6f39dca5321fd820e2c82baa495a2c52a3c04854
Danilo <danilo.colloca91@gmail.com>
2021-03-29 15:06:00 +0200

    inizializzazione laravel/tableAuthor migrate

---
+++ database/migrations/2021_03_29_114116_create_authors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 80);
+            $table->string('lastname', 80);
+            $table->string('mail', 2048);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+}
